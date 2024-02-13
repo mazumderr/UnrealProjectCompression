4a1aa2534adff87f65fc8f1d1dfba2cdd2aad280
mazumderr <135399797+mazumderr@users.noreply.github.com>
2024-02-12 22:32:56 -0800

    Add files via upload

--- UnrealProjectCompression.ps1
+++ UnrealProjectCompression.ps1
@@ -18,10 +18,15 @@ $DestinationPath = "$LabPath" + " Copy"
 New-Item -Path $DestinationPath -ItemType Directory
 
 # Make a backup of the lab files 
+Write-Host "Making a backup of your files..."
 Copy-Item -Path "$LabPath\*" -Destination "$DestinationPath" -Recurse
 
 # Compress it all 
+Write-Host "Compressing..."
 Compress-Archive -LiteralPath "$DestinationPath" -DestinationPath "$Env:UnrealPath\$CompressName.zip"
 
 # Delete backup
+Write-Host "Cleaning up files..."
 Remove-Item "$DestinationPath" -Recurse
+
+Write-Host "Done!"
